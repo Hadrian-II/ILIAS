2a26b8534bac9066975eab5e3f2236a47bac74fa
Adrian Lüthi <adi.l@bluewin.ch>
2020-09-07 19:20:52 +0200

    read renderers on artifact generation to increase performance

--- src/UI/Setup/class.ilUIBuildRendererMappingObjective.php
+++ src/UI/Setup/class.ilUIBuildRendererMappingObjective.php
@@ -4,6 +4,13 @@ namespace ILIAS\UI\Setup;
 
 use ILIAS\Setup;
 use ILIAS\UI\Implementation\Render\ComponentRenderer;
+use ILIAS\UI\Implementation\Render\JavaScriptBinding;
+use ILIAS\UI\Implementation\Render\TemplateFactory;
+use ILIAS\UI\Implementation\Render\ilTemplateWrapperFactory;
+use ILIAS\UI\Implementation\Factory as UIFactory;
+use ILIAS\Refinery\Factory as RefineryFactory;
+use ilLanguage;
+use ILIAS\UI\Implementation\Render\ilJavaScriptBinding;
 
 /**
  * Class ilGSBootLoaderBuilder
@@ -14,6 +21,31 @@ class ilUIBuildRendererMappingObjective extends Setup\Artifact\BuildArtifactObje
 {
     const ARTIFACT_PATH = 'src/UI/artifacts/core_renderers.php';
 
+    /**
+     * @var	UIFactory
+     */
+    private $ui_factory;
+
+    /**
+     * @var	TemplateFactory
+     */
+    private $tpl_factory;
+
+    /**
+     * @var	ilLanguage
+     */
+    private $lng;
+
+    /**
+     * @var	JavaScriptBinding
+     */
+    private $js_binding;
+
+    /**
+     * @var RefineryFactory
+     */
+    private $refinery;
+
     public function getArtifactPath() : string
     {
         return self::ARTIFACT_PATH;
@@ -22,10 +54,41 @@ class ilUIBuildRendererMappingObjective extends Setup\Artifact\BuildArtifactObje
 
     public function build() : Setup\Artifact
     {
+        $this->ui_factory = new class() extends UIFactory { public function __construct() {} };
+        $this->tpl_factory = new class() extends ilTemplateWrapperFactory { public function __construct() {} };
+        $this->lng = new class() extends ilLanguage { public function __construct() {} };
+        $this->js_binding = new class() extends ilJavaScriptBinding { public function __construct() {} };
+        $this->refinery = new class() extends RefineryFactory { public function __construct() {} };
+
         $interface_name = ComponentRenderer::class;
         $interface_finder = new Setup\ImplementationOfInterfaceFinder($interface_name);
-        $class_names = iterator_to_array($interface_finder->getMatchingClassNames());
 
-        return new Setup\Artifact\ArrayArtifact($class_names);
+        $mapping = [];
+
+        foreach ($interface_finder->getMatchingClassNames() as $renderer_class)
+        {
+            $renderer = $this->createRenderer($renderer_class);
+
+            foreach ($renderer->getComponentInterfaceName() as $component_class) {
+                $mapping[$component_class] = $renderer_class;
+            }
+        }
+
+        return new Setup\Artifact\ArrayArtifact($mapping);
+    }
+
+    /**
+     * @param string $renderer_class
+     * @return ComponentRenderer
+     */
+    private function createRenderer(string $renderer_class) : ComponentRenderer
+    {
+        return new $renderer_class(
+            $this->ui_factory,
+            $this->tpl_factory,
+            $this->lng,
+            $this->js_binding,
+            $this->refinery
+            );
     }
 }
